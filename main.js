964259e3e254dc88651b3623bcd7937e96645daa
João Paulo Buchholz <51836804+Jompa14@users.noreply.github.com>
2019-12-18 21:11:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,49 @@
 //DOM Elements
-
-const time = document.getElementById("time"),
-const greeting = document.getElementById("greeting"),
-const name = document.getElementById("name"),
+console.log ("Grêmio")
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
 const focus = document.getElementById("focus");
 
 //Function Show Time
 
-function showTime(); {
- let today = new Date(),
- hour = today.getHours (),
- min = todey.getMinutes ();
- 
- 
+function showTime() {
+ const today = new Date();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
  //Output time
- 
- time.innerHTML = `${addZero(hour)}<span>:</span>${adZero(min)}`;
+ document.getElementById("time").innerHTML = `${addZero(hour)}<span>:</span>${addZero(minute)}`;
  
  setTimeout(showTime, 1000);
+ console.log("hello");
  }
  
  //add Zeros
  
- function addZero(n); {
+ function addZero(n) {
  return (parseInt (n, 10) < 10 ? "0" : " ") + n;
  }
  
  //Set Background and greeting
  
- function setBgGreeting(); {
+ function setBgGreeting() {
   let today = new Date;
   hour = today.getHours (),
   
   if (hour > 6 && < 13) {
-  document.body.style.backgroundImage = "url(xxxxxxxxxxxxx)";
+  document.body.style.backgroundImage = "/home/jompa/LandingPage/images/coffee.jpeg";
   greeting.textContent = "Good morning";
-  }   else if (hour > 13 && < 19) {
-  document.body.style.backgroundImage = "url(xxxxxxxxxxxxx)";
+  }   else if (hour < 19) {
+  document.body.style.backgroundImage = "/home/jompa/LandingPage/images/chimarrao.jpeg";
   greeting.textContent = "Good afternoon";
   }   else ()
-  
-  // compleate the good eveening 
+  document.body.style.backgroundImage = "/home/jompa/LandingPage/images/city.jpeg";
+  greeting.textContent = "Good evening";
   
  }
  
  //Run
  
- showTime();
+ moment();
+ setBgGreeting();
  
