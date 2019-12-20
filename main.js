cf72ccc89c3a0846ceb56daf3ceb54b23ef83277
João Paulo Buchholz <51836804+Jompa14@users.noreply.github.com>
2019-12-19 23:17:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,100 @@
-//DOM Elements
-console.log ("Grêmio")
-const time = document.getElementById("time");
-const greeting = document.getElementById("greeting");
-const name = document.getElementById("name");
-const focus = document.getElementById("focus");
+// DOM Elements
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  focus = document.getElementById('focus');
 
-//Function Show Time
+// Options
+const showAmPm = true;
 
+// Show Time
 function showTime() {
- const today = new Date();
-  let hour = today.getHours();
-  let minute = today.getMinutes();
- //Output time
- document.getElementById("time").innerHTML = `${addZero(hour)}<span>:</span>${addZero(minute)}`;
- 
- setTimeout(showTime, 1000);
- }
- 
- //add Zeros
- 
- function addZero(n) {
- return (parseInt (n, 10) < 10 ? "0" : " ") + n;
- }
- 
- //Set Background and greeting
- 
- function setBgGreeting() {
-  let today = new Date;
-  hour = today.getHours (),
-  
-  if (hour > 6 && < 13) {
-  document.body.style.backgroundImage = "/home/jompa/LandingPage/images/coffee.jpeg";
-  greeting.textContent = "Good morning";
-  }   else if (hour < 19) {
-  document.body.style.backgroundImage = "/home/jompa/LandingPage/images/chimarrao.jpeg";
-  greeting.textContent = "Good afternoon";
-  }   else ()
-  document.body.style.backgroundImage = "/home/jompa/LandingPage/images/city.jpeg";
-  greeting.textContent = "Good evening";
- }
- }
- 
- //Run
- 
- showTime();
- setBgGreeting();
- 
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(); 
+
+  // Output Time
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}`;
+
+  setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+    greeting.textContent = 'Good Morning, ';
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon, ';
+  } else {
+    // Evening
+    document.body.style.backgroundImage = "images/nightlight.jpeg";
+    greeting.textContent = 'Good Evening, ';
+  }
+}
+
+// Get Name
+function getName() {
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
+
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Focus]';
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
+}
+
+// Set Focus
+function setFocus(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+  }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
+// Run
+showTime();
+setBgGreet();
+getName();
+getFocus();
